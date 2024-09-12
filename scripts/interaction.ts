51214ff47406f3f5f92fcd0705cdf2bacb2af41d
Yusuf Roqib <114329528+yusufroqib@users.noreply.github.com>
2024-09-12 14:17:42 +0100

    deposit fund to first multisig wallet

--- scripts/interaction.ts
+++ scripts/interaction.ts
@@ -1,12 +1,22 @@
 import hre, { ethers } from "hardhat";
-import { MultisigFactory, MultisigFactory__factory } from "../typechain-types";
+import {
+	Multisig,
+	MultisigFactory,
+	MultisigFactory__factory,
+	Web3CXI,
+} from "../typechain-types";
 import { ContractTransactionResponse } from "ethers";
 
 async function main() {
 	const MultisigFactoryAddress = "0xCf7Ed3AccA5a467e9e704C703E8D87F634fB0Fc9";
+	const Web3CXIAddress = "0x2279B7A0a67DB372996a5FaB50D91eAA73d2eBe6";
 	const multisigFactory: MultisigFactory = await ethers.getContractAt(
 		"MultisigFactory",
 		MultisigFactoryAddress
+	);
+	const web3CXI: Web3CXI = await ethers.getContractAt(
+		"Web3CXI",
+		Web3CXIAddress
 	);
 	const [owner, otherAccount, thirdAccount, invalidSigner] =
 		await hre.ethers.getSigners();
@@ -19,7 +29,7 @@ async function main() {
 	const multisigClonesBeforeCreating =
 		await multisigFactory.getMultiSigClones();
 	console.log(
-		"Multisig clones before creating new",
+		"Multisig clones before creating new wallet",
 		multisigClonesBeforeCreating
 	);
 	const CreateWalletTxn = await multisigFactory.createMultisigWallet(
@@ -30,9 +40,19 @@ async function main() {
 
 	await CreateWalletTxn.wait();
 
-
-
-	
+	const multisigClonesAfterCreating = await multisigFactory.getMultiSigClones();
+	console.log(
+		"Multisig clones after creating new wallet",
+		multisigClonesAfterCreating
+	);
+	const firstMultisig = multisigClonesAfterCreating[0];
+	const multisig: Multisig = await ethers.getContractAt(
+		"Multisig",
+		firstMultisig
+	);
+	const amount = ethers.parseUnits("100", 18);
+	const depositTx = await web3CXI.transfer(multisig, amount);
+	console.log({ depositTx });
 }
 // async function getEvent(
 // 	contract: OnChainNFT,
